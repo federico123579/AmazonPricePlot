5d4d3090ad1ce16555f83967ae26c901d6d256ea
Federico Lolli <federico123579@gmail.com>
2016-11-11 19:08:41 +0100

    resolved last bugs

--- AmazonPricePlot.py
+++ AmazonPricePlot.py
@@ -1,5 +1,6 @@
 import sys                              # Import from the folder modules
 sys.path.insert(0, 'modules')
-import dataCollector
+import dataCollector, grapher
 
 dataCollector.write_data(dataCollector.collect_data())
+grapher.linear_graph("data/price-data.txt", "Amazon Prices", "amazon-price-date", dataCollector.collect_data())
---
+++ modules/grapher.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import plotly.graph_objs as go
+import plotly.plotly as py
+def linear_graph(link_to_file, title, filename, range_file):
+    py.sign_in('federico123579', 'tmp58hnp2w')
+    # Import data from csv
+    df = pd.read_csv(link_to_file)
+    data = []
+    for count, val in enumerate(range_file):
+        data.append(go.Scatter(
+            x = df['Date'],
+            y = df[val['name']],
+            mode = 'lines+markers',
+            name = val['name']
+        ))
+    layout = go.Layout(title=title)
+    fig = go.Figure(data=data, layout=layout)
+
+    # Plot data in the notebook
+    py.plot(fig, filename=filename)
