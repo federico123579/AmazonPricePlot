750dd6dfc13f77faefc64ceb6b1747f779ba5fd7
federico123579 <federico123579@gmail.com>
2016-11-12 22:08:43 +0100

    WIP: work in progress

--- PricePlot.py
+++ PricePlot.py
@@ -2,5 +2,5 @@ import sys                              # Import from the folder modules
 sys.path.insert(0, 'modules')
 import dataCollector, grapher
 dataCollector.update_data("data/data.txt", "data/list.txt")
-dataCollector.write_data(dataCollector.collect_data())
-grapher.linear_graph("data/price-data.txt", "Amazon Prices", "amazon-price-date", dataCollector.collect_data())
+dataCollector.write_data(dataCollector.collect_data("data/data.txt"), "data/price-data.txt")
+grapher.linear_graph("data/price-data.txt", "Amazon Prices", "amazon-price-date", dataCollector.collect_data("data/data.txt"))
